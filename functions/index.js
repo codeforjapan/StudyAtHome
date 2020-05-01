280d401a5318660faf59c4c1e4dde6564a6accbd
Nekoya3 <taiyu31@gmail.com>
2020-05-02 03:14:48 +0900

    Fix:Package.json

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,21 @@
 const functions = require('firebase-functions')
+const express = require('express')
+const { Nuxt } = require('nuxt')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express()
+const config = {
+  dev: false,
+  buildDir: '.nuxt',
+  build: {
+    publicPath: '/assets/',
+  },
+}
+const nuxt = new Nuxt(config)
+
+async function handleRequest(req, res) {
+  res.set('Cache-Control', 'public, max-age=300, s-maxage=600')
+  await nuxt.ready() // ← nuxt.ready()でawaitしないといけなくなった！！
+  return nuxt.render(req, res)
+}
+app.use(handleRequest)
+exports.ssr = functions.https.onRequest(app)
