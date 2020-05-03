ab633807f4c558e131848f67387aa6148c34a2d5
Nekoya3 <taiyu31@gmail.com>
2020-05-04 02:25:53 +0900

    FIX: BackGround Color ADD: index Logo

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,28 +1,31 @@
 <template>
   <div class="MainPage">
+    <div class="Logo">
+      <Logo />
+    </div>
     <v-row class="DataBlock">
-      <v-col cols="12" sm="6">
+      <v-col cols="12" md="6">
         <StudyCard
           schooltime="1"
           realtime="9:00 - 10:00"
           content="内容（1時間目！！！！！）"
         />
       </v-col>
-      <v-col cols="12" sm="6">
+      <v-col cols="12" md="6">
         <StudyCard
           schooltime="2"
           realtime="10:00 - 11:00"
           content="内容（2時間目！！！！！）"
         />
       </v-col>
-      <v-col cols="12" sm="6">
+      <v-col cols="12" md="6">
         <StudyCard
           schooltime="3"
           realtime="12:00 - 13:00"
           content="内容（3時間目！！！！！）"
         />
       </v-col>
-      <v-col cols="12" sm="6">
+      <v-col cols="12" md="6">
         <StudyCard
           schooltime="4"
           realtime="14:00 - 15:00"
@@ -35,13 +38,17 @@
 
 <script>
 import StudyCard from '@/components/StudyCard'
+import Logo from '@/assets/svgs/logo.svg'
 export default {
-  components: { StudyCard },
+  components: { StudyCard, Logo },
 }
 </script>
 
 <style lang="scss" scoped>
 .MainPage {
+  .Logo {
+    text-align: center;
+  }
   .DataBlock {
     margin: 20px -12px;
     .studycard {
