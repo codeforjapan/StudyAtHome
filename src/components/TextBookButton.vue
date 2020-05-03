3c860bfc2712900f46ea363a01e354e205742bfc
Nekoya3 <taiyu31@gmail.com>
2020-05-04 02:12:18 +0900

    Make: Schedule Card

---
+++ src/components/TextBookButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="contentButton">
+    <iconBook class="icon" />
+    <div class="button-string">
+      <ruby>教<rt>きょう</rt>科<rt>か</rt>書<rt>しょ</rt></ruby>
+    </div>
+  </div>
+</template>
+
+<script>
+import iconBook from '@/assets/svgs/icon_book.svg'
+export default {
+  name: 'ContentsButton',
+  components: {
+    iconBook,
+  },
+}
+</script>
+
+<style scoped>
+.contentButton {
+  display: flex;
+  padding: 10px;
+  font-family: 'Noto Sans', sans-serif;
+  height: 3em;
+  align-items: center;
+  justify-content: center;
+  font-weight: bold;
+  background: #ffffff;
+  border: 1px solid #bdc3c7;
+  box-sizing: border-box;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.15);
+  border-radius: 4px;
+}
+.icon {
+  margin-right: 10px;
+}
+.button-string {
+  width: 7em;
+  text-align: center;
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,13 +1,52 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <h1>test</h1>
-    </v-flex>
-  </v-layout>
+  <div class="MainPage">
+    <v-row class="DataBlock">
+      <v-col cols="12" sm="6">
+        <StudyCard
+          schooltime="1"
+          realtime="9:00 - 10:00"
+          content="内容（1時間目！！！！！）"
+        />
+      </v-col>
+      <v-col cols="12" sm="6">
+        <StudyCard
+          schooltime="2"
+          realtime="10:00 - 11:00"
+          content="内容（2時間目！！！！！）"
+        />
+      </v-col>
+      <v-col cols="12" sm="6">
+        <StudyCard
+          schooltime="3"
+          realtime="12:00 - 13:00"
+          content="内容（3時間目！！！！！）"
+        />
+      </v-col>
+      <v-col cols="12" sm="6">
+        <StudyCard
+          schooltime="4"
+          realtime="14:00 - 15:00"
+          content="内容（4時間目！！！！！）"
+        />
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
-export default {}
+import StudyCard from '@/components/StudyCard'
+export default {
+  components: { StudyCard },
+}
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.MainPage {
+  .DataBlock {
+    margin: 20px -12px;
+    .studycard {
+      margin-bottom: 20px;
+    }
+  }
+}
+</style>
