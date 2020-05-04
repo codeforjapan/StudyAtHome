3c9e74e9a5048a58f0c646e4ef9e966a8c3d4e56
Nekoya3 <taiyu31@gmail.com>
2020-05-04 23:52:13 +0900

    FIX: Env Setting

--- nuxt.config.js
+++ nuxt.config.js
@@ -109,6 +109,16 @@ module.exports = {
       },
     },
   },
+  env: {
+    APIKEY: process.env.APIKEY,
+    AUTHDOMAIN: process.env.AUTHDOMAIN,
+    DATABASEURL: process.env.DATABASEURL,
+    PROJECTID: process.env.PROJECTID,
+    STORAGEBUCKET: process.env.STORAGEBUCKET,
+    MESSAGINGSENDERID: process.env.MESSAGINGSENDERID,
+    APPID: process.env.APPID,
+    MEASUREMENTID: process.env.MEASUREMENTID,
+  },
   /*
    ** Build configuration
    */
