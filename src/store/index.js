f6a8680ba72d0d950708c8c998d76e9329435ca2
Nekoya3 <taiyu31@gmail.com>
2020-05-04 21:03:47 +0900

    FIX: firestore to vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,44 +1,14 @@
-import cloneDeep from 'lodash.clonedeep'
-
 export const state = () => ({
-  user: null,
   users: [],
-  posts: [],
-  isFetching: false,
 })
 
 export const getters = {
-  user: (state) => state.user,
-  posts: (state) => state.posts,
-  isFetching: (state) => state.isFetching,
+  users: (state) => state.users,
 }
 
 export const mutations = {
-  setUser(state, { user }) {
-    state.user = cloneDeep(user)
-  },
   setUsers(state, { users }) {
     state.users = users
-  },
-  setPosts(state, { posts }) {
-    state.posts = [...posts]
-  },
-  addPost(state, { post }) {
-    if (state.posts.find((p) => p.id === post.id)) {
-      return
-    }
-    post.user = state.users.find((user) => user.email === post.from)
-    state.posts = [...state.posts, post]
-  },
-  unshiftPost(state, { post }) {
-    if (state.posts.find((p) => p.id === post.id)) {
-      return
-    }
-    post.user = state.users.find((user) => user.email === post.from)
-    state.posts = [post, ...state.posts]
-  },
-  setIsFetching(state, next) {
-    state.isFetching = !!next
   },
 }
 
@@ -57,50 +27,11 @@ export const mutations = {
 
 export const actions = {
   async nuxtServerInit({ commit }) {
-    const posts = []
     const users = []
-    const [usersSnapshot, postsSnapshot] = await Promise.all([
-      this.$firestore.collection('users').get(),
-      this.$firestore
-        .collection('posts')
-        .orderBy('createdAt', 'desc')
-        .limit(20)
-        .get(),
-    ])
+    const usersSnapshot = await this.$firestore.collection('users').get()
     usersSnapshot.forEach((user) => {
       users.push(user.data())
     })
-    postsSnapshot.forEach((postSnapshot) => {
-      const post = postSnapshot.data()
-      post.user = users.find((user) => user.email === post.from)
-      posts.unshift(post)
-    })
     commit('setUsers', { users })
-    commit('setPosts', { posts })
-  },
-  async fetchPosts({ commit, state }) {
-    if (state.setIsFetching) return
-    commit('setIsFetching', true)
-    try {
-      const posts = []
-      const postsSnapshot = await this.$firestore
-        .collection('posts')
-        .orderBy('createdAt', 'desc')
-        .startAfter(state.posts[0].createdAt)
-        .limit(20)
-        .get()
-      postsSnapshot.forEach((postSnapshot) => {
-        const post = postSnapshot.data()
-        post.user = state.users.find((user) => user.email === post.from)
-        posts.push(post)
-      })
-      posts.forEach((post) => {
-        commit('unshiftPost', { post })
-      })
-      return posts[0]
-    } catch (e) {
-    } finally {
-      commit('setIsFetching', false)
-    }
   },
 }
