c62b9f52a130eb36506821f98b38022239e56289
Nekoya3 <taiyu31@gmail.com>
2020-05-04 23:22:11 +0900

    Functions test

--- functions/index.js
+++ functions/index.js
@@ -3,11 +3,14 @@ const { Nuxt } = require('nuxt')
 const express = require('express')
 const app = express()
 
-const envs = functions.config().environment
-
-Object.entries(envs).forEach((k, v) => {
-  process.env[`${k}`.toUpperCase()] = v
-})
+process.env.STORAGEBUCKET = functions.config().environment.storagebucket
+process.env.MESSAGINGSENDERID = functions.config().environment.messagingsenderid
+process.env.AUTHDOMAIN = functions.config().environment.authdomain
+process.env.APIKEY = functions.config().environment.apikey
+process.env.DATABASEURL = functions.config().environment.databaseurl
+process.env.APPID = functions.config().environment.appid
+process.env.PROJECTID = functions.config().environment.projectid
+process.env.MEASUREMENTID = functions.config().environment.measurementid
 
 const config = {
   dev: false,
@@ -26,6 +29,6 @@ function handleRequest(req, res) {
     })
   })
 }
-console.log(functions.config().environment.apikey)
+console.log(process.env)
 app.use(handleRequest)
 exports.ssr = functions.https.onRequest(app)
