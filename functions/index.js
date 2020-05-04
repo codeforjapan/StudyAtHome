ba358a80fed202ff4e162c190ac6d15fb6a5f72a
Nekoya3 <taiyu31@gmail.com>
2020-05-05 01:30:29 +0900

    FIX: ServerScript

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,6 @@
 const functions = require('firebase-functions')
 const { Nuxt } = require('nuxt')
 const express = require('express')
-const app = express()
 
 process.env.STORAGEBUCKET = functions.config().environment.storagebucket
 process.env.MESSAGINGSENDERID = functions.config().environment.messagingsenderid
@@ -12,6 +11,7 @@ process.env.APPID = functions.config().environment.appid
 process.env.PROJECTID = functions.config().environment.projectid
 process.env.MEASUREMENTID = functions.config().environment.measurementid
 
+const app = express()
 const config = {
   dev: false,
   buildDir: '.nuxt',
@@ -21,14 +21,10 @@ const config = {
 }
 const nuxt = new Nuxt(config)
 
-function handleRequest(req, res) {
-  res.set('Cache-Control', 'public, max-age=10, s-maxage=10')
-  return new Promise((resolve, reject) => {
-    nuxt.render(req, res, (promise) => {
-      promise.then(resolve).catch(reject)
-    })
-  })
+async function handleRequest(req, res) {
+  res.set('Cache-Control', 'public, max-age=300, s-maxage=600')
+  await nuxt.ready() // ← nuxt.ready()でawaitしないといけなくなった！！
+  return nuxt.render(req, res)
 }
-
 app.use(handleRequest)
 exports.ssr = functions.https.onRequest(app)
