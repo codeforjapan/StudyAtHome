f17ba5f347063c6200c37e31642de4452977dbd5
Nekoya3 <taiyu31@gmail.com>
2020-05-07 01:49:41 +0900

    FIX: AuthのCookie上の維持

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,10 @@
+import Cookies from 'js-cookie'
+import firebase from '@/plugins/firebase.js'
+
 export const state = () => ({
   user: null,
   info: null,
+  uid: null,
 })
 
 export const mutations = {
@@ -9,6 +13,13 @@ export const mutations = {
       state.user = payload
     } else {
       state.user = null
+    }
+  },
+  saveUid(state, payload) {
+    if (payload) {
+      state.uid = payload
+    } else {
+      state.uid = null
     }
   },
   setInfo(state, payload) {
@@ -21,8 +32,33 @@ export const mutations = {
 }
 
 export const actions = {
+  async login({ dispatch, state }, user) {
+    console.log('userlogin')
+    const token = await firebase.auth().currentUser.getIdToken(true)
+    const userInfo = {
+      name: user.displayName,
+      email: user.email,
+      avatar: user.photoURL,
+      uid: user.uid,
+    }
+
+    Cookies.set('access_token', token) // saving token in cookie for server rendering
+    await dispatch('setUser', userInfo)
+    await dispatch('saveUid', userInfo.uid)
+  },
+
+  async logout({ commit }) {
+    await firebase.auth().signOut()
+
+    Cookies.remove('access_token')
+    commit('setUser', null)
+    commit('saveUid', null)
+  },
   setUser({ commit }, payload) {
     commit('setUser', payload)
+  },
+  saveUid({ commit }, payload) {
+    commit('saveUid', payload)
   },
   setInfo({ commit }, payload) {
     commit('setInfo', payload)
@@ -35,5 +71,11 @@ export const getters = {
   },
   info: (state) => {
     return state.info
+  },
+  uid: (state) => {
+    return state.uid
+  },
+  isAuthenticated: (state) => {
+    return !!state.user && !!state.user.uid
   },
 }
