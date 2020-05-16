b038801174f4b564541cb921edbf69fc20c3e425
y-chan <y_chan_crypto@yahoo.co.jp>
2020-05-16 16:31:52 +0900

    add user.ts

---
+++ src/store/modules/user.ts
@@ -0,0 +1,68 @@
+import {
+  VuexModule,
+  Module,
+  action,
+  getter,
+  mutation
+} from 'vuex-class-component'
+import Cookies from 'js-cookie'
+import firebase from 'firebase'
+import { User } from 'firebase'
+
+interface UserDataState {
+  userData: any
+  uid: string | null
+}
+
+@Module({ namespacedPath: 'modules/class/', target: 'nuxt' })
+class UserData extends VuexModule implements UserDataState {
+  @getter userData: any = null
+  @getter uid: string | null = null
+  @getter isAuthenticated: boolean = !!this.userData && !!this.uid
+
+  @mutation
+  public setUserData(payload: any) {
+    if (payload) {
+      this.userData = payload
+    } else {
+      this.userData = null
+    }
+  }
+
+  @mutation
+  public setUid(payload: string | null) {
+    if (payload) {
+      this.uid = payload
+    } else {
+      this.uid = null
+    }
+  }
+
+  @action
+  public async login() {
+    const userData: User | null = await firebase.auth().currentUser
+    const token: Promise<string> | null = userData ? userData.getIdToken(true) : null
+    if (userData !== null && token !== null) {
+      Cookies.set('__session', token) // saving token in cookie for server rendering
+      const userD: firebase.firestore.DocumentSnapshot = await firebase
+        .firestore()
+        .collection('users')
+        .doc(userData.uid)
+        .get()
+      const allowAccess: string = userD.get('allow_access')
+      await this.setUserData({ allow_access: allowAccess })
+      await this.setUid(userData.uid)
+    }
+  }
+
+  @action
+  public async logout() {
+    await firebase.auth().signOut()
+
+    Cookies.remove('__session')
+    this.setUserData(null)
+    this.setUid(null)
+  }
+}
+
+export default UserData.ExtractVuexModule(UserData)
