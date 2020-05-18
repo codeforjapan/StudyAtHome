9dfb8d352906048724c065ef6b6a99a21b638266
MaySoMusician <maysomusician@gmail.com>
2020-05-18 16:34:19 +0900

    Use vxm

--- src/pages/classes/index.vue
+++ src/pages/classes/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="MainPage">
-    <v-row v-if="lessons[displayDate]" class="DataBlock">
+    <v-row v-if="classData.lessons[classData.displayDate]" class="DataBlock">
       <v-col
-        v-for="(item, i) in lessons[displayDate]"
+        v-for="(item, i) in classData.lessons[classData.displayDate]"
         :key="i"
         cols="12"
         md="6"
@@ -17,7 +17,7 @@
     </v-row>
 
     <v-row
-      v-else-if="displayDate === this.$dayjs().format('YYYY-MM-DD')"
+      v-else-if="classData.displayDate === this.$dayjs().format('YYYY-MM-DD')"
       class="DataBlock"
     >
       <h1 style="color: white; width: 100vw; text-align: center;">
@@ -26,7 +26,7 @@
     </v-row>
     <v-row v-else class="DataBlock">
       <h1 style="color: white; width: 100vw; text-align: center;">
-        {{ this.$dayjs(displayDate).format('M/D') }}
+        {{ this.$dayjs(classData.displayDate).format('M/D') }}
         の時間割はまだ届いていないみたいです
       </h1>
     </v-row>
@@ -36,19 +36,13 @@
 <script lang="ts">
 import Vue from 'vue'
 import { vxm } from '@/store'
-import { Lesson } from '@/store/modules/class'
 import StudyCard from '@/components/StudyCard.vue'
 export default Vue.extend({
   components: { StudyCard },
   layout: 'classes',
-  computed: {
-    lessons(): {
-      [key: string]: Lesson
-    } {
-      return vxm.class.classData.lessons
-    },
-    displayDate(): string {
-      return vxm.class.displayDate
+  data() {
+    return {
+      classData: vxm.classData
     }
   }
 })
