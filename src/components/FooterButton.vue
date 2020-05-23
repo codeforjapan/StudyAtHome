612843ab7fc0fd0f41d3e2d9a2a92afc8435aedc
Kota Yatagai <kouta.giants0706@gmail.com>
2020-05-23 14:39:34 +0000

    +ボタンの実装(未完)

---
+++ src/components/FooterButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-btn height="3.5em" class="FooterButton">
+    <iconBook class="icon" />
+  </v-btn>
+</template>
+
+<script>
+import Vue from 'vue'
+import iconFooter from '@/assets/svgs/icon_plus.svg'
+export default {
+  name: 'FooterButton',
+  components: {
+    iconFooter
+  }
+}
+</script>
+
+<style scoped>
+</style>
