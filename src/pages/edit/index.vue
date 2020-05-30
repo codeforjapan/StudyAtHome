161db769ac87b8d60fafca9eca905f6bd5278e05
Nekoya3 <taiyu31@gmail.com>
2020-05-30 15:51:46 +0900

    loginとclassesページをstoreの変更に沿う形に変更

--- src/pages/edit/index.vue
+++ src/pages/edit/index.vue
@@ -1,12 +1,18 @@
 <template>
-  <h1>Editor</h1>
+  <h1>{{ email }}</h1>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { vxm } from '@/store'
 
 export default Vue.extend({
   layout: 'protected',
-  name: 'EditorIndex'
+  name: 'EditorIndex',
+  data() {
+    return {
+      email: vxm.user.email
+    }
+  }
 })
 </script>
