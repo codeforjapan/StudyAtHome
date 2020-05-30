0a63dd1cbdae9c88b5eacfa8e5ab47962ab8d52d
Nekoya3 <taiyu31@gmail.com>
2020-05-30 15:53:39 +0900

    AuthにCookieを使わないため、cookieを利用しない方法に変更

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import Vuex, { ActionContext } from 'vuex'
-import { Context } from '@nuxt/types/app'
 import { createProxy, extractVuexModule } from 'vuex-class-component'
-import { getUserFromCookie } from '@/helpers/index.js'
+import firebase from '@/plugins/firebase'
 import { UserStore } from '@/store/modules/user'
 import { ClassDataStore } from '@/store/modules/classData'
 
@@ -16,16 +15,12 @@ export const store = new Vuex.Store({
 })
 
 export const actions = {
-  nuxtServerInit(_ctx: ActionContext<any, any>, { req }: Context) {
-    const user = getUserFromCookie(req)
-    if (user) {
-      vxm.user.userData = {
-        name: user.name,
-        email: user.email,
-        avatar: user.picture,
-        uid: user.user_id
+  nuxtServerInit(_ctx: ActionContext<any, any>) {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        vxm.user.login()
       }
-    }
+    })
   }
 }
 
