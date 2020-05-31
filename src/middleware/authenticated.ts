51a1ac158d36dc270ea07f2fdde61ec332612de1
Nekoya3 <taiyu31@gmail.com>
2020-05-31 13:26:56 +0900

    middlewareではvuexのデータを確認するように変更

--- src/middleware/authenticated.ts
+++ src/middleware/authenticated.ts
@@ -1,16 +1,10 @@
 import { Middleware } from '@nuxt/types'
-import firebase from '@/plugins/firebase.js'
 import { vxm } from '@/store'
 
 const authenticated: Middleware = async ({ redirect }) => {
-  firebase.auth().onAuthStateChanged(user => {
-    if (user) {
-      vxm.user.login()
-    } else {
-      vxm.user.logout()
-      redirect('/account/login')
-    }
-  })
+  if (!vxm.user.isAuthenticated) {
+    redirect('/account/login')
+  }
 }
 
 export default authenticated
