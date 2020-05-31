10200951bd25df389b49f00271ada1fad6c7d03f
Nekoya3 <taiyu31@gmail.com>
2020-05-31 21:40:40 +0900

    各種ヘッダーの設定

--- src/layouts/classes.vue
+++ src/layouts/classes.vue
@@ -1,124 +1,66 @@
 <template>
   <v-app>
-    <v-overlay v-if="loading" color="#F8F9FA" opacity="1" z-index="9999">
+    <v-overlay v-if="loading" color="#0071C2" opacity="1" z-index="9999">
       <div class="loader">
         Loading
       </div>
     </v-overlay>
-    <!--
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    -->
-    <v-app-bar
-      fixed
-      app
-      dense
-      class="bar"
-      style="background-color: rgba(0, 0, 0, 0)"
-      elevation="0"
-    >
-      <span>
-        {{ classData.schoolName }}
-      </span>
+    <v-app-bar fixed app class="bar" elevation="0">
+      <HeaderLogo />
       <v-spacer />
-      <div class="date">
-        <v-btn icon small dark @click="classData.prevDate">
-          <v-icon>mdi-chevron-left</v-icon>
-        </v-btn>
-        <v-menu
-          ref="menu"
-          v-model="menu"
-          :close-on-content-click="false"
-          :return-value.sync="classData.displayDate"
-          transition="scale-transition"
-          offset-y
-        >
-          <template v-slot:activator="{ on }">
-            <v-btn text dark style="padding: 0 0;" v-on="on">
-              {{ classData.displayDate }}
-            </v-btn>
-          </template>
-          <v-date-picker v-model="classData.displayDate" no-title scrollable>
-            <v-spacer />
-            <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
-            <v-btn
-              text
-              color="primary"
-              @click="$refs.menu.save(classData.displayDate)"
-            >
-              OK
-            </v-btn>
-          </v-date-picker>
-        </v-menu>
-        <v-btn icon small dark @click="classData.nextDate">
-          <v-icon>mdi-chevron-right</v-icon>
-        </v-btn>
-      </div>
-      <span>
-        {{ classData.className }}
-      </span>
+      <v-btn outlined rounded color="#0071C2">
+        ログアウト
+        <v-icon right>mdi-exit-to-app</v-icon>
+      </v-btn>
+      <template v-slot:extension>
+        <div class="header-calender">
+          <CalendarBar />
+        </div>
+      </template>
     </v-app-bar>
     <v-content class="content">
       <v-container class="px-4 py-8">
         <nuxt />
       </v-container>
     </v-content>
-    <v-footer dark color="#0071c2">
-      <span>&copy; Code For Japan {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import dayjs from 'dayjs'
-import { vxm } from '@/store'
+import HeaderLogo from '@/assets/svgs/header_logo.svg'
+import CalendarBar from '@/components/CalendarBar.vue'
 
 type LocalData = {
   loading: boolean
-  menu: boolean
-  classData: typeof vxm.classData
 }
 
 export default Vue.extend({
   middleware: 'checkClassData',
+  components: {
+    CalendarBar,
+    HeaderLogo
+  },
   data(): LocalData {
     return {
-      loading: true,
-      menu: false,
-      classData: vxm.classData
+      loading: true
     }
   },
   mounted(): void {
     this.loading = false
-    this.classData.displayDate = dayjs().format('YYYY-MM-DD')
   }
 })
 </script>
 
 <style scoped lang="scss">
+.v-btn {
+  font-family: 'Noto Sans', 'Noto Sans JP', sans-serif;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 12px;
+  line-height: 16px;
+  background-color: $color-white;
+}
 .date-icon {
   margin-right: 15px;
 }
@@ -129,11 +71,16 @@ export default Vue.extend({
   transform: translate(-50%, -50%);
 }
 .bar {
-  background-color: rgba(0, 0, 0, 0);
-  color: white;
+  background-color: $color-back-gray;
   text-align: center;
 }
 .content {
   background-color: $color-base-color-01;
+}
+.header-calender {
+  margin: 0 auto;
+  width: 100%;
+  max-width: 640px;
+  height: 40px;
 }
 </style>
