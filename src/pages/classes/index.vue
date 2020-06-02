3e60acb9798d54eb2104210e9822acdcd7d27a93
Nekoya3 <taiyu31@gmail.com>
2020-06-02 16:16:50 +0900

    その日のデータを表示するように変更

--- src/pages/classes/index.vue
+++ src/pages/classes/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="MainPage">
-    <v-row v-if="classData.getLessonsByDisplayDate" class="DataBlock">
+    <v-row v-if="classData.getLessonsByDisplayDate.length" class="DataBlock">
       <v-col
         v-for="(item, i) in classData.getLessonsByDisplayDate"
         :key="i"
@@ -15,7 +15,7 @@
       </v-col>
     </v-row>
 
-    <v-row v-else-if="isToday" class="DataBlock">
+    <v-row v-else-if="today" class="DataBlock">
       <h1 style="color: white; width: 100vw; text-align: center;">
         今日の時間割はまだ届いていないみたいです
       </h1>
@@ -31,36 +31,31 @@
 <script lang="ts">
 import Vue from 'vue'
 import dayjs from 'dayjs'
+import isToday from 'date-fns/isToday'
 import { vxm } from '@/store'
 import ContentCard from '@/components/ContentCard.vue'
 
 type Data = {
   classData: typeof vxm.classData
-}
-
-type Computed = {
-  isToday: boolean
+  today: boolean
   dateTitle: string
 }
 
-export default Vue.extend<Data, unknown, Computed, unknown>({
+export default Vue.extend({
   components: { ContentCard },
   layout: 'classes',
-  data() {
+  data(): Data {
     return {
-      classData: vxm.classData
+      classData: vxm.classData,
+      today: true,
+      dateTitle: '1/1'
     }
   },
-  computed: {
-    isToday() {
-      return (
-        dayjs(this.classData.displayDate).format('YYYY-MM-DD') ===
-        dayjs().format('YYYY-MM-DD')
-      )
-    },
-    dateTitle() {
-      return dayjs(this.classData.displayDate).format('M/D')
-    }
+  mounted() {
+    vxm.classData.$subscribe('setDate', () => {
+      this.today = isToday(this.classData.displayDate)
+      this.dateTitle = dayjs(this.classData.displayDate).format('M/D')
+    })
   },
   methods: {
     formatDate(date: Date): string {
