970a51e39c29b69bd5b61aac8c60a424afb7f325
Kazuki Imamura <kaizumaki@gmail.com>
2020-06-09 17:36:26 +0900

    [WIP] 利用規約ページを作成

---
+++ src/pages/terms.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <action-button theme="primary" text="利用を開始する" />
+    <action-button theme="border" text="同意しない" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import ActionButton from '@/components/ActionButton.vue'
+
+export default Vue.extend({
+  components: { ActionButton },
+  layout: 'default'
+})
+</script>
