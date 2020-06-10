b143c4bc73e177267d52c7929e9c9fa4d4bfa6aa
Kazuki Imamura <kaizumaki@gmail.com>
2020-06-10 10:32:10 +0900

    編集ボタンつきのコンテンツカードを作成

---
+++ src/components/ContentCardEditable.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="ContentCard-Outer">
+    <v-card class="ContentCard elevation-4">
+      <v-card-actions class="ContentCard-Actions">
+        <subject-tag
+          v-for="(item, idx) in subjects"
+          :key="idx"
+          class="ContentCard-SubjectTag"
+          :name="item.name || '教科名'"
+          :icon="item.icon || ''"
+          :icon-color="item.iconColor || 'white'"
+          :background-color="item.backgroundColor || '#A5D8FF'"
+        />
+        <subject-tag
+          class="ContentCard-SubjectTag"
+          :name="'動画'"
+          :icon="'mdi-video'"
+          :icon-color="'#424242'"
+          :background-color="'#E0E0E0'"
+        />
+      </v-card-actions>
+      <v-card-title>
+        <h2 class="ContentCard-Title">{{ title }}</h2>
+      </v-card-title>
+      <v-card-text>
+        <p class="ContentCard-Description">{{ description }}</p>
+      </v-card-text>
+    </v-card>
+    <div class="ContentCard-Button-Outer">
+      <content-card-editor-button
+        class="ContentCard-Button"
+        icon-name="mdi-eye-off"
+      />
+      <content-card-editor-button
+        class="ContentCard-Button"
+        icon-name="mdi-pencil"
+      />
+      <content-card-editor-button
+        class="ContentCard-Button"
+        icon-name="mdi-file-multiple"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import SubjectTag from '@/components/SubjectTag.vue'
+import ContentCardEditorButton from '@/components/ContentCardEditorButton.vue'
+
+export default Vue.extend({
+  components: {
+    SubjectTag,
+    ContentCardEditorButton
+  },
+  props: {
+    subjects: {
+      type: Array,
+      default: () => []
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    description: {
+      type: String,
+      default: ''
+    }
+  }
+})
+</script>
+
+<style scoped lang="scss">
+.ContentCard-Outer {
+  display: flex;
+}
+.ContentCard-Button-Outer {
+  display: flex;
+  flex-direction: column;
+  flex: 0 1 auto;
+  margin-left: 12px;
+  .ContentCard-Button {
+    margin-bottom: 12px;
+  }
+}
+.ContentCard {
+  flex: 1 1 auto;
+  color: $color-gray;
+  border-radius: 14px !important;
+  .ContentCard-Actions {
+    padding: 16px 16px 0;
+  }
+  .ContentCard-SubjectTag {
+    margin-right: 4px;
+  }
+  .ContentCard-Title {
+    font-weight: bold;
+    font-size: 14px;
+    line-height: 1.75;
+  }
+  .ContentCard-Description {
+    font-size: 12px;
+    color: $color-gray;
+    margin: 0;
+  }
+}
+</style>
