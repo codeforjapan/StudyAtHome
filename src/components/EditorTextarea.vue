eed2c0a4b8b4f12783b4b853722ad4f4c232c590
Kazuki Imamura <kaizumaki@gmail.com>
2020-06-11 09:52:44 +0900

    EditorTextareaからアイコンボタンを削除

--- src/components/EditorTextarea.vue
+++ src/components/EditorTextarea.vue
@@ -1,37 +1,28 @@
 <template>
   <div>
     <span v-if="title" class="EditorField-Title">{{ title }}</span>
-    <div class="EditorField-Form">
-      <v-textarea
-        v-model="value"
-        :hint="hint"
-        :label="label"
-        :placeholder="placeholder"
-        background-color="white"
-        class="elevation-0"
-        solo
-        flat
-        outlined
-      />
-      <content-card-editor-button
-        v-if="iconName"
-        class="Button"
-        :icon-name="iconName"
-      />
-    </div>
+    <v-textarea
+      v-model="value"
+      :hint="hint"
+      :label="label"
+      :placeholder="placeholder"
+      background-color="white"
+      class="Textarea"
+      solo
+      flat
+      outlined
+    />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import ContentCardEditorButton from '@/components/ContentCardEditorButton.vue'
 
 type DataType = {
   value: string
 }
 
 export default Vue.extend({
-  components: { ContentCardEditorButton },
   props: {
     title: {
       type: String,
@@ -52,11 +43,6 @@ export default Vue.extend({
       type: String,
       required: false,
       default: ''
-    },
-    iconName: {
-      type: String,
-      required: false,
-      default: ''
     }
   },
   data(): DataType {
@@ -67,7 +53,7 @@ export default Vue.extend({
 })
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .EditorField-Title {
   display: block;
   font-size: 16px;
@@ -75,16 +61,7 @@ export default Vue.extend({
   color: $color-white;
   margin-bottom: 4px;
 }
-.EditorField-Form {
-  display: flex;
-
-  .Textarea {
-    box-shadow: inset 2px 2px 5px rgba(0, 0, 0, 0.3);
-  }
-
-  .Button {
-    flex: 0 1 auto;
-    margin-left: 8px;
-  }
+.v-input__slot {
+  box-shadow: inset 2px 2px 5px rgba(0, 0, 0, 0.3);
 }
 </style>
