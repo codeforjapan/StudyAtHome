0ce205f634595c6760d129ff45ed333ac8c3d252
Nekoya3 <taiyu31@gmail.com>
2020-06-12 14:06:34 +0900

    EditingScreen1にformDataを追加

--- src/components/EditingScreen1.vue
+++ src/components/EditingScreen1.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <editor-field
+      v-model="formData.date"
       title="日付設定"
       label="date"
       placeholder="XX月XX日"
@@ -9,6 +10,7 @@
     />
     <div class="EditingScreen-Flex EditingScreen-Time">
       <editor-field
+        v-model="formData.startTime"
         title="時間設定"
         label="start_time"
         placeholder="00:00"
@@ -18,6 +20,7 @@
       />
       <span class="Hyphen">-</span>
       <editor-field
+        v-model="formData.endTime"
         label="end_time"
         placeholder="00:00"
         :transparent="true"
@@ -28,12 +31,14 @@
     <editor-field title="タイトル" label="title" placeholder="例）理科" />
     <div class="EditingScreen-Flex">
       <editor-field
+        v-model="formData.subjectName"
         title="教科名"
         label="lesson"
         placeholder="例）理科"
         class="LessonField"
       />
       <editor-field
+        v-model="formData.subjectColor"
         title="ラベル色"
         icon-name="mdi-palette"
         class="LabelField"
@@ -47,7 +52,18 @@ import Vue from 'vue'
 import EditorField from '~/components/EditorField.vue'
 
 export default Vue.extend({
-  components: { EditorField }
+  components: { EditorField },
+  data() {
+    return {
+      formData: {
+        date: '',
+        startTime: '',
+        endTime: '',
+        subjectName: '',
+        subjectColor: ''
+      }
+    }
+  }
 })
 </script>
 
