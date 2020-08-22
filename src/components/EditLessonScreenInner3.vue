deb79afeaa25da824e7f37fb4d6efdda51b02acb
Souta Ito <soutaito@info-lounge.jp>
2020-08-22 17:10:15 +0900

    [WIP] Search videos fron NHK For School data

--- src/components/EditLessonScreenInner3.vue
+++ src/components/EditLessonScreenInner3.vue
@@ -6,6 +6,23 @@
       label="video"
       placeholder="https://"
     />
+
+    <editor-input-field-pickable
+      v-model="videoSearchWord"
+      :title="$t('components.editing_screen.labels.video_keyword')"
+      :placeholders="$t('components.editing_screen.placeholder.video_keyword')"
+    />
+
+    <div v-if="videoSearchResult.length > 0">
+      <h3>NHK For Schoolの動画検索結果</h3>
+      <ul>
+        <li v-for="(v, i) in videoSearchResult" :key="i">
+          <a :href="v.videoUrl" target="_blank">
+            {{ v.videoTitle }}
+          </a>
+        </li>
+      </ul>
+    </div>
     <video-thumbnail
       v-if="tempFormData.videoTitle && tempFormData.videoThumbnailUrl"
       :caption="tempFormData.videoTitle"
@@ -20,11 +37,14 @@ import { Vue, Component, Prop, Emit, Watch } from 'vue-property-decorator'
 import VideoThumbnail from '@/components/VideoThumbnail.vue'
 import EditorInputFieldPickable from '~/components/EditorInputFieldPickable.vue'
 
+let movies: any[] = []
+
 export type formData = {
   videoUrl: string
   videoTitle: string
   videoThumbnailUrl: string
 }
+
 @Component({
   components: {
     EditorInputFieldPickable,
@@ -49,6 +69,17 @@ export default class EditLessonScreenInner3 extends Vue {
   })
   public value!: formData
 
+  videoSearchWord: string = ''
+  videoSearchResult: formData[] = []
+
+  mounted() {
+    fetch('/data/movies.json')
+      .then((res) => res.json())
+      .then((data) => {
+        movies = data
+      })
+  }
+
   private get form(): formData {
     return this.value
   }
@@ -56,6 +87,26 @@ export default class EditLessonScreenInner3 extends Vue {
   @Watch('tempFormData', { deep: true })
   onChangeTempFormData() {
     this.input(this.tempFormData)
+  }
+
+  @Watch('videoSearchWord')
+  onChangeVideoSearchWord() {
+    if (this.videoSearchWord) {
+      this.videoSearchResult = movies
+        .filter((v) => {
+          const fullText = Object.keys(v)
+            .map((k) => v[k])
+            .join()
+          return fullText.includes(this.videoSearchWord)
+        })
+        .map((v) => {
+          return {
+            videoUrl: v['教材_ID'],
+            videoTitle: v['教材_タイトル'],
+            videoThumbnailUrl: '',
+          }
+        })
+    }
   }
 
   /* CORS 回避必須
